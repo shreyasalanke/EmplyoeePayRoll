eb9e0710abe6e057a071a7bbdf8d589a225036c5
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-25 11:33:57 +0530

    Initial Commit: Welcome to Emplyoee Pay Roll Service

---
+++ lib/src/main/java/com/bridgelabz/emplyoeepayroll/EmplyoeePayRollMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.emplyoeepayroll;
+
+public class EmplyoeePayRollMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Emplyoee Pay Roll Service");
+	}
+}
